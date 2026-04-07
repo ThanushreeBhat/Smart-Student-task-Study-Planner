f57685a83a36ef3642e18bc39a2ead11f0d9e09a
Thanushree <tsbhat306@gmail.com>
2026-04-07 11:48:05 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+app.listen(5000, () => console.log("Server running"));
