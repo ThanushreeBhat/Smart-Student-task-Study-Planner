80e07b928e450840095b3c72acfc2c11749b23bc
Thanush <thanush5489gmail.com>
2026-04-27 21:22:05 +0530

    pages and services done

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import API from "../services/api";
 
-export default function Login() {
+export default function Register() {
   const [form, setForm] = useState({});
 
-  const login = async () => {
-    const res = await API.post("/login", form);
-    localStorage.setItem("token", res.data.token);
-    window.location.href = "/dashboard";
+  const register = async () => {
+    await API.post("/register", form);
+    alert("Registered!");
   };
 
   return (
     <div>
+      <input placeholder="Name" onChange={e => setForm({...form, name: e.target.value})}/>
       <input placeholder="Email" onChange={e => setForm({...form, email: e.target.value})}/>
       <input placeholder="Password" onChange={e => setForm({...form, password: e.target.value})}/>
-      <button onClick={login}>Login</button>
+      <button onClick={register}>Register</button>
     </div>
   );
 }
